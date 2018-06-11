990dee71c6f9682679328430e50794899504833d
Dmitr0k <dkukhno@gmail.com>
2018-06-11 07:05:39 +0300

    added gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var   gulp = require('gulp'),
 	browserSync = require("browser-sync").create();
 
-gulp.task("server", function () {
-	browserSync.init({
-		server: { baseDir: './app/' }
+// gulp.task("server", function () {
+// 	browserSync.init({
+// 		server: { baseDir: './app/' }
+// 	});
+//       gulp.watch('app/**/*.html').on('change', browserSync.reload);
+//       gulp.watch('app/**/*.css').on('change', browserSync.reload);
+// });
+
+gulp.task('serveprod', function() {
+	connect.server({
+	  root: ['./app/'],
+	  port: process.env.PORT || 5000, // localhost:5000
+	  livereload: false
 	});
-      gulp.watch('app/**/*.html').on('change', browserSync.reload);
+	gulp.watch('app/**/*.html').on('change', browserSync.reload);
       gulp.watch('app/**/*.css').on('change', browserSync.reload);
-});
+    });
 
-gulp.task('default', ['server']);
-
-
+gulp.task('default', ['serveprod']);
