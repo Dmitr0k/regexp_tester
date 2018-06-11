ae0f6f6546d958504ae2b213f51a7e9b051d6753
Dmitr0k <dkukhno@gmail.com>
2018-06-11 07:45:23 +0300

    some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 var   gulp = require('gulp'),
 	browserSync = require("browser-sync").create();
 
-// gulp.task("server", function () {
-// 	browserSync.init({
-// 		server: { baseDir: './app/' }
-// 	});
-//       gulp.watch('app/**/*.html').on('change', browserSync.reload);
-//       gulp.watch('app/**/*.css').on('change', browserSync.reload);
-// });
-
-gulp.task('serveprod', function() {
-	connect.server({
-	  root: ['./app/'],
-	  port: process.env.PORT || 5000, // localhost:5000
-	  livereload: false
+gulp.task("server", function () {
+	browserSync.init({
+		server: { baseDir: './app/' },
+		port: process.env.PORT || 5000, // localhost:5000
 	});
-	gulp.watch('app/**/*.html').on('change', browserSync.reload);
+      gulp.watch('app/**/*.html').on('change', browserSync.reload);
       gulp.watch('app/**/*.css').on('change', browserSync.reload);
-    });
+});
 
-gulp.task('default', ['serveprod']);
+// gulp.task('serveprod', function() {
+// 	connect.server({
+// 	  root: ['./app/'],
+// 	  port: process.env.PORT || 5000, // localhost:5000
+// 	  livereload: false
+// 	});
+// 	gulp.watch('app/**/*.html').on('change', browserSync.reload);
+//       gulp.watch('app/**/*.css').on('change', browserSync.reload);
+//     });
+
+gulp.task('default', ['server']);
